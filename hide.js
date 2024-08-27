5bb9bf23aa83826f95c786930f61348db28895d1
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-08-27 12:59:43 +0900

    Update hide.js

--- hide.js
+++ hide.js
@@ -1,1 +1,1 @@
-javascript:(function(){const e=document.querySelectorAll('*');e.forEach(el=>el.style.cursor=el.style.cursor==='none'?'':'none');})();
+javascript:(function(){ const elements = document.querySelectorAll('*'); elements.forEach(el => el.style.cursor = el.style.cursor === 'none' ? '' : 'none'); })();
